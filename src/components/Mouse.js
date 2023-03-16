381010b3cb15688719bb8fa8f33045d6ccb80d05
Adminka <nsokolova0399@mail.ru>
2023-03-17 00:28:35 +0300

    Adding touch events

--- src/components/Mouse.js
+++ src/components/Mouse.js
@@ -17,10 +17,6 @@ function Mouse() {
         function moveAt(e) {
             square.style.left = (e.pageX - square.offsetWidth / 2) + 'px';
             square.style.top = (e.pageY - square.offsetHeight / 2) + 'px';
-            console.log('border x ', border.getBoundingClientRect().x)
-            console.log('border with + x  ', border.getBoundingClientRect().x + border.getBoundingClientRect().width / 2)
-            console.log(e.pageX)
-            console.log(e.pageX - square.offsetWidth / 2)
             if (square.getBoundingClientRect().x > border.getBoundingClientRect().x - square.offsetWidth
                 && square.getBoundingClientRect().x < border.getBoundingClientRect().x + border.getBoundingClientRect().width
                 && square.getBoundingClientRect().y > border.getBoundingClientRect().y - square.offsetHeight
@@ -35,10 +31,17 @@ function Mouse() {
         document.onmousemove = function (e) {
             moveAt(e);
         }
+        document.ontouchmove = function (e) {
+            moveAt(e);
+        }
 
         square.onmouseup = function () {
             document.onmousemove = null;
             square.onmouseup = null;
+        }
+        square.ontouchend = function () {
+            document.ontouchmove = null;
+            square.ontouchend = null;
         }
         square.style.width = '13rem'
     }
@@ -49,10 +52,10 @@ function Mouse() {
 
             <div className="main-puzzle">
                 <div className="main-block1-square">
-                    <img className="item-1" alt="" src="image/rectangle1.svg" onMouseDown={onmousedown} style={{}}/>
-                    <img className="item-2" alt="" src="image/rectangle2.svg" onMouseDown={onmousedown} style={{}}/>
-                    <img className="item-3" alt="" src="image/rectangle3.svg" onMouseDown={onmousedown} style={{}}/>
-                    <img className="item-4" alt="" src="image/rectangle4.svg" onMouseDown={onmousedown} style={{}}/>
+                    <img className="item-1" alt="" src="image/rectangle1.svg" onMouseDown={onmousedown} onTouchStart={onmousedown} style={{}}/>
+                    <img className="item-2" alt="" src="image/rectangle2.svg" onMouseDown={onmousedown} onTouchStart={onmousedown} style={{}}/>
+                    <img className="item-3" alt="" src="image/rectangle3.svg" onMouseDown={onmousedown} onTouchStart={onmousedown} style={{}}/>
+                    <img className="item-4" alt="" src="image/rectangle4.svg" onMouseDown={onmousedown} onTouchStart={onmousedown} style={{}}/>
                 </div>
                 <div className="main-block2">
                     <div className="main-border"></div>
